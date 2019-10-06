ee05d9665e17a29f61a64180f54fc449cd065b83
Evan Krone <evankrone@Evans-MBP-2.fios-router.home>
2019-10-06 18:00:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const superbowlWin = (record) => {
+let r = record.find( record => record.result === "W" )
+if (r !== undefined){
+return r.year;
 }
+else {
+  return r;
+  }
+}
+
